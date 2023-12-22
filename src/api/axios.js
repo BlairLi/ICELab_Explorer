75df07926d9512a4bfe1de440254a6897d920526
YucanLi <1579193739@qq.com>
2023-12-22 03:52:42 -0500

    Changes to be committed: 	modified:   src/api/axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 // const BASE_URL = 'http://Cisc498-env.eba-r9edeqnn.us-east-1.elasticbeanstalk.com';
 // const BASE_URL = 'http://Cisc498testing-env.eba-tjww5nka.us-east-1.elasticbeanstalk.com';
 // const BASE_URL = 'http://498autentication-env.eba-w49fafyt.us-east-1.elasticbeanstalk.com';
-const BASE_URL = 'http://localhost:3501';
+// const BASE_URL = 'http://localhost:3501';https://relaxed-squirrel-43e63d.netlify.app
+const BASE_URL = 'https://relaxed-squirrel-43e63d.netlify.app';
 // const BASE_URL = 'http://localhost:3000';
 // const BASE_URL = 'https://testing.d33784y2zyhj3.amplifyapp.com/';
 // const BASE_URL = 'http://52.91.184.60';
