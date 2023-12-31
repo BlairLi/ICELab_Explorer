19387729ad62da297628d5d99bb6f24f6a305db7
YucanLi <1579193739@qq.com>
2023-12-31 16:02:32 -0500

    Changes to be committed: 	modified:   config/allowedOrigins.js 	modified:   src/component/Login.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,13 +1,13 @@
-// const allowedOrigins = [
-//     'http://127.0.0.1:5500',
-//     'http://localhost:3500',
-//     'http://localhost:3000',
-//     'http://192.168.2.11:3000',
-//     'https://icelab-explorer.netlify.app',
-//     'https://master.dbs29o8fuwcy2.amplifyapp.com',
-//     'https://65854511b418e40008300307--bucolic-druid-71c9aa.netlify.app',
-//     'https://master--bucolic-druid-71c9aa.netlify.app'
-// ];
-const allowedOrigins = [];
+const allowedOrigins = [
+    'http://127.0.0.1:5500',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://192.168.2.11:3000',
+    'https://icelab-explorer.netlify.app',
+    'https://master.dbs29o8fuwcy2.amplifyapp.com',
+    'https://65854511b418e40008300307--bucolic-druid-71c9aa.netlify.app',
+    'https://master--bucolic-druid-71c9aa.netlify.app'
+];
+// const allowedOrigins = [];
 
 module.exports = allowedOrigins;
--- src/component/Login.js
+++ src/component/Login.js
@@ -38,8 +38,8 @@ const Login = () => {
                 withCredentials: true
             }
         );
-        // console.log(JSON.stringify(response?.data));
-        // console.log(JSON.stringify(response));
+        console.log(JSON.stringify(response?.data));
+        console.log(JSON.stringify(response));
         const accessToken = response?.data?.accessToken;
         const roles = response?.data?.roles;
         setAuth({ user, pwd, roles, accessToken });
