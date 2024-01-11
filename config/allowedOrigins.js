babba0692007873849f488b65ebd5a6f0a031062
YucanLi <1579193739@qq.com>
2024-01-11 10:44:30 -0500

    Changes to be committed: 	modified:   .env 	modified:   config/allowedOrigins.js 	modified:   src/api/axios.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -5,7 +5,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://192.168.2.11:3000',
     'https://icelab-explorer.netlify.app',
-    'https://vercel-icel-ab.vercel.app',
+    'https://ice-lab-explorer.vercel.app',
     'https://master.dbs29o8fuwcy2.amplifyapp.com',
     'https://65854511b418e40008300307--bucolic-druid-71c9aa.netlify.app',
     'https://master--bucolic-druid-71c9aa.netlify.app'
